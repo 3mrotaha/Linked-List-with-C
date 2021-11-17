b525d32ac063a3efd5e0410713ba97496cf92917
amr12601 <62963089+amr12601@users.noreply.github.com>
2021-11-17 23:23:26 +0200

    Definitions, Types Definitions, Prototypes

--- prototypes.h
+++ prototypes.h
@@ -1,26 +1,33 @@
 /************************************************************************** 
- * Auther : Amr Mohamed Taha Ibrahem                                      *
- * Version : V1                                                           *
- * DATE : 14th nov 2021                                                   *
+ * Auther : Amr M. Taha                                                   *
+ * Version : V2.0                                                         *
+ * DATE : 17th nov 2021                                                   *
  * Description :  Linked List - prototypes                                *
  **************************************************************************/
  
+ #ifndef PROTOTYPES_H
+ #define PROTOTYPES_H
  
+ /****************************************************************** 
+  * To control the date being read by the user                     *
+  ******************************************************************/
+ #define DATA_READ_PLACEHOLDER	"%f"
  
+ /****************************************************************** 
+  * To control the Display of data stored in the linked list       *
+  ******************************************************************/
+ #define DATA_PRINT_PLACEHOLDER	"%.3f "
  
- /*************************************************************** 
-  * Defining the Node Type                                      *
-  ***************************************************************/
- struct node{
-	 float data;
-	 struct node* next;
- };
- 
+ /****************************************************************** 
+  * Type of the data going to be stored in the linked list         *
+  ******************************************************************/
+ typedef float DataType;
+  
  /****************************************************************** 
   * Addfirst : This function is used to add element to be the      *
   *			  first element in the Linked List.                    *
   ******************************************************************/
- void Addfirst(float value);
+ void Addfirst(DataType value);
  
  /******************************************************************
   * Deletefirst : This function is used to Delete the first        *
@@ -32,7 +39,7 @@
   * AddLast : This function is used to add element to be the       *
   *			 last element in the Linked List.                      *
   ******************************************************************/
- void AddLast(float value);
+ void AddLast(DataType value);
  
  /******************************************************************
   * DeleteLast : This function is used to Delete the last          *
@@ -44,13 +51,19 @@
   * AddBefore : This function is used to Add an element before     *
   *				an existed element in the Linked List.             *
   ******************************************************************/
- void AddBefore(float Element, float value);
+ void AddBefore(DataType Element, DataType value);
  
  /******************************************************************
   * DeleteElement : This function is used to Delete any            *
   *			 	element in the Linked List.                        *
   ******************************************************************/
- void DeleteElement(float Element);
+ void DeleteElement(DataType Element);
+ 
+ /******************************************************************
+  * AddByIndex : This function is used to Add an element before    *
+  *				Corresponding Index in the Linked List.            *
+  ******************************************************************/
+ void AddByIndex(const int Index, DataType value);
  
  /******************************************************************
   * DisplayList : This function is used to Display the elements    *
@@ -58,5 +71,6 @@
   ******************************************************************/
  void DisplayList(void);
  
+ #endif
  
  
