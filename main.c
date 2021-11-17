c65f56b8b18707bcb7c5cddab5811ff17358cb21
amr12601 <62963089+amr12601@users.noreply.github.com>
2021-11-17 23:21:43 +0200

    Program Main

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
 /************************************************************************** 
- * Auther : Amr Mohamed Taha Ibrahem                                      *
- * Version : V1                                                           *
- * DATE : 14th nov 2021                                                   *
+ * Auther : Amr M. Taha                                                   *
+ * Version : V2.0                                                         *
+ * DATE : 17th nov 2021                                                   *
  * Description :  Linked List - main                                      *
  **************************************************************************/
  #include <stdio.h>
 #include "prototypes.h"
 
 int main(){
-	float data;
+	DataType data;
 	int command;
-	printf("\ncommands : \n>> 1 to add first, 2 to delete first\n>> 3 to add last, 4 to delete last \n>> 5 to add before an existed element, 6 to delete any element\n>> 0 to exit :");
+	printf("\ncommands : \n>> 1 to add first, 2 to delete first\n>> 3 to add last, 4 to delete last \n>> 5 to add before an existed element, 6 to delete any element\n>> 7 to Add by Index\n>> 0 to exit");
 	while(1){
 		printf("\nEnter a command : ");
 		scanf("%d", &command);
 		
 		if(!command){break;}
 		else if(command == 1){
-			scanf("%f", &data);
+			scanf(DATA_READ_PLACEHOLDER, &data);
 			Addfirst(data);
 			DisplayList();
 		}
@@ -26,7 +26,7 @@ int main(){
 			DisplayList();
 		}
 		else if(command == 3){
-			scanf("%f", &data);
+			scanf(DATA_READ_PLACEHOLDER, &data);
 			AddLast(data);
 			DisplayList();
 		}
@@ -35,19 +35,28 @@ int main(){
 			DisplayList();
 		}
 		else if(command == 5){
-			float Element;
+			DataType Element;
 			printf("Element to Add Before : ");
-			scanf("%f", &Element);
+			scanf(DATA_READ_PLACEHOLDER, &Element);
 			printf("Data to be added : ");
-			scanf("%f", &data);
+			scanf(DATA_READ_PLACEHOLDER, &data);
 			AddBefore(Element, data);
 			DisplayList();
 		}
 		else if(command == 6){
-			float Element;
+			DataType Element;
 			printf("Element to be deleted : ");
-			scanf("%f", &Element);
+			scanf(DATA_READ_PLACEHOLDER, &Element);
 			DeleteElement(Element);
+			DisplayList();
+		}
+		else if(command == 7){
+			int Index;
+			printf("Enter Index : ");
+			scanf("%d", &Index);
+			printf("Data to be added : ");
+			scanf(DATA_READ_PLACEHOLDER, &data);
+			AddByIndex(Index, data);
 			DisplayList();
 		}
 	}
